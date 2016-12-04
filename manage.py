7fc1d8b9875fa584396d5b6001830771de8af435
Banyi <1181395975@qq.com>
2016-12-05 00:36:15 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -13,24 +13,13 @@ from app import login_manager
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-# mail = Mail(app)  # 邮件测试
+mail = Mail(app)  # 邮件测试
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
-
-# def connect_db():
-#    return sqlite3.connect(app.config['DATABASE'])
-#
-#
-# def init_db():
-#     with closing(connect_db()) as db:
-#         with app.open_resource('schema.sql', mode='r') as f:
-#             db.cursor().executescript(f.read())
-#         db.commit()
 
 @manager.command
 def test():
